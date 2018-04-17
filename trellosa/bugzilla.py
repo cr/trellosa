e14aeda46b100a606b5c50538da2ef722ed5cf36
Frederik Braun <fbraun@mozilla.com>
2018-04-17 15:15:11 +0200

    implemented bugzilla.get_snapshot()

--- trellosa/bugzilla.py
+++ trellosa/bugzilla.py
@@ -14,10 +14,11 @@ logger = logging.getLogger(__name__)
 
 
 def generate_token_url():
-    return "https://bugzilla.mozilla.org/machmirntoken"
+    return "https://bugzilla.mozilla.org/userprefs.cgi?tab=apikey"
 
 
 class BugzillaClient(object):
+    URL = "https://bugzilla.mozilla.org/rest/bug"
 
     def __init__(self, token=None):
         self.token = token
@@ -27,17 +28,28 @@ class BugzillaClient(object):
         return generate_token_url()
 
     def check_token(self, token):
-        # FIXME: implement proper check
         return type(token) is str and token.isalnum() and 30 < len(token) < 50
 
     def set_token(self, token):
         self.token = token
 
     def get_snapshot(self):
-        # FIXME: needs implementation
+        params = {
+            "include_fields": "_all", # could also be "id,summary,status,url,version,target_milestone"
+            "component": "Security: Review Requests",
+            "product": "Firefox",
+            "api_key": self.token
+        }
+        response = requests.get(self.URL, params=params)
+        if not response.status_code == 200:
+            raise HTTPError("Status was not 200. Meh.")
+
+        bugs = response.json()['bugs']
+        if len(bugs) == 0:
+            raise HTTPError("Could not find any bugs at all.")
+
         now = time.time()
         meta = {"snapshot_time": now}
-        bugs = []
 
         return {"meta": meta, "bugs": bugs, "whatever": "else"}
 
